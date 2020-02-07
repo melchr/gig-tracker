43737c506808ee55ef37bea1325ce43c506bc646
Mel <melchrstmn@gmail.com>
2020-02-07 11:28:45 -0500

    added helpers file for controlling logic

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,18 @@
+
+class Helpers
+
+    def self.is_logged_in?(session)
+      if session[:user_id] != nil
+        true
+      else
+        false
+      end
+    end
+  
+    def self.current_user(session)
+      if self.is_logged_in?(session)
+        @user = User.find_by_id(session[:user_id])
+      end
+    end
+  
+  end
