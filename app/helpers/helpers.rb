db2c66ef8223acb8a2d310bdc288ce9608096d1b
Mel <melchrstmn@gmail.com>
2020-02-10 15:42:37 -0500

    commented out, put helpers in main controller

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,18 +1,16 @@
 
-class Helpers
+#class Helpers
 
-    def self.is_logged_in?(session)
-      if session[:user_id] != nil
-        true
-      else
-        false
-      end
-    end
-  
-    def self.current_user(session)
-      if self.is_logged_in?(session)
-        @user = User.find_by_id(session[:user_id])
-      end
-    end
-  
-  end
+ # def logged_in?
+ #   !!session[:user_id]
+ # end#
+
+ # def current_user
+ #   @current_user ||= User.find_by(id: session[:user_id])
+ # end
+
+ # def can_edit?(gig)
+ #   gig.user == current_user
+ # end
+
+#end
