b443289d0dd75ed658cf2206e03b12f18aa1086c
Mel <melchrstmn@gmail.com>
2020-02-10 16:14:35 -0500

    changed current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   def current_user
-    User.find(session[:user_id])
+    @user ||= User.find_by_id(session[:user_id])
   end
 
   def is_authorized?(id)
