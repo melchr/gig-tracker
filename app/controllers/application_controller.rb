c5fb0bd5f0299b25dd96bcc71f9dcca0c800d6cf
Mel <melchrstmn@gmail.com>
2020-02-10 15:40:20 -0500

    changed methods bc kept getting an error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,146 +9,38 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
   end
 
-  get "/signup" do
-    erb :'/users/signup'
-  end
-
-  post "/signup" do
-    if User.find_by(:email => params["email"])
-      puts "Sorry, there is already an account using this email address"
-      redirect "/login"
-    else
-      user = User.create(params)
-
-      if user.save
-        session[:user_id] = user.id
-        redirect "/"
-      else
-        puts "Sorry. All fields must be filled out."
-        redirect "/signup"
-      end
-
-    end
-  #  @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-   # @user.save
-   #@user = User.new(params)
-   # @user.save
-    #session[:user_id] = @user.id
-    #puts params
-    #redirect '/users/home'
-    #session[:name] = params[:name]
-  end
-
-  get "/login" do
-    erb :'users/login'
-  end
-
-  post '/login' do
-    @user = User.find_by(:email => params["email"])
-    if @user && @user.authenticate(params["password"])
-      session[:user_id] = @user.id
-      redirect "/home"
-    end
-    redirect "/login"
-  end
-
-  get '/home' do
-    @user = User.find(session[:user_id])
-    erb :'users/home'
-  end
-
-  get '/gigs' do
-    if Helpers.is_logged_in?(session)
-      @user = Helpers.current_user(session)
-      @gigs = Gig.all
-      #if @user != nil
-      #  @gigs = Gig.where(:user_id => @user.id)
-      erb :'gigs/index'
-    else
-      redirect "/login"
-    end
-  end
-
-  post '/gigs' do
-    user = Helpers.current_user(session)
-
-    gig = user.gigs.create(params)
-    if gig.save
-      puts "Gig has been saved."
-    else
-      puts "Please try again."
-    end
-
-    redirect "/gigs"
-  end
-
-get '/gigs/new' do
-  if Helpers.is_logged_in?(session)
-    erb :'gigs/new'
-  else
-    redirect "/login"
-  end
-end
-
-get '/gigs/:id' do
-  if Helpers.is_logged_in?(session)
-    @user = Helpers.current_user(session)
-    @gig = Gig.find_by_id(params["id"])
-    if @gig.id == @gig.user_id
-      erb :'gigs/show'
-    else
-      redirect "/login"
-    end
-  else
-    redirect "/login"
-  end
-end
-
-get '/gigs/:id/edit' do
-  if Helpers.is_logged_in?(session)
-    @user = Helpers.current_user(session)
-    @gig = Gig.find_by_id(params[:id]) #["id"]
-    if @user.id == @gig.user_id
-      erb :'gigs/edit'
-    else
-      redirect "/login"
-    end
-  else
-    redirect "/login"
-  end
-end
-
-patch '/gigs/:id' do
-  @user = Helpers.current_user(session)
-  @gig = Gig.find_by_id(params["id"])
-
-  if @user.id == @gig.user_id
-    @gig.update(params["gig"])
-    redirect "/gigs/#{@gig.id}"
-  else
-    redirect "/login"
-  end
-end
-
-delete '/gigs/:id' do
-  @user = Helpers.current_user(session)
-  @gig = Gig.find_by_id(params["id"])
-
-  if @user.id == @gig.user_id
-    @gig.destroy
-    redirect "/gigs"
-  else
-    redirect "/login"
-  end
-end
 
   get '/logout' do
     session.clear
-    redirect "/"
+    redirect to '/'
   end
+
+  #helpers
+  helpers do
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def is_authorized?(id)
+    current_user.id == id.to_i
+  end
+
+ # def not_logged_in
+  #  if !logged_in?
+ #     "You must be logged in to view that page."
+ #     redirect '/'
+ #   end
+ # end
+
+end
 
 end
