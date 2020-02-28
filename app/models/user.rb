745fa62e36cdab72923b1379b530fee292405cb8
Mel <melchrstmn@gmail.com>
2020-02-28 14:33:31 -0500

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :gigs
-    validates :name, :email, :password_digest, presence: true
-
+    validates :name, :email, :password_digest, presence: true 
+  
   end
