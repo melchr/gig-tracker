c7166c8b9f1c9a94cfdce182a5a9a9c49da91c57
Mel <melchrstmn@gmail.com>
2020-02-28 14:19:37 -0500

    deleted unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,34 @@
 require './config/environment'
 
-class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Base 
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, 'app/views' 
     enable :sessions
-    set :session_secret, "mels_session"
+  
+    set :session_secret, "mels_session" 
   end
 
-  get '/' do
-    erb :welcome
+  get '/' do 
+    erb :welcome 
   end
 
 
-  get '/logout' do
-    session.clear
-    redirect to '/'
+  get '/logout' do 
+    session.clear 
+    redirect to '/' 
   end
 
-  #helpers
   helpers do
 
   def logged_in?
-    !!current_user
+    !!current_user 
   end
 
   def current_user
-    @user ||= User.find_by_id(session[:user_id])
+    @user ||= User.find_by_id(session[:user_id]) 
   end
-
- # def not_logged_in
-  #  if !logged_in?
- #     "You must be logged in to view that page."
- #     redirect '/'
- #   end
- # end
 
 end
 
